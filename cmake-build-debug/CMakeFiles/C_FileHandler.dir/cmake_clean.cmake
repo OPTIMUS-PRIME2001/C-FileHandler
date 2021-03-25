5b5121b4be92aa0bef53bf264023893fc224ac3d
Shatanik Mahanty <shatanikmahanty@gmail.com>
2021-03-25 18:56:49 +0530

    Added C-Make to project for build and packaging.

---
+++ cmake-build-debug/CMakeFiles/C_FileHandler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_FileHandler.dir/FileSTU.cpp.obj"
+  "C_FileHandler.exe"
+  "C_FileHandler.exe.manifest"
+  "C_FileHandler.lib"
+  "C_FileHandler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_FileHandler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
